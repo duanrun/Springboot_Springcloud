d84a2ab4aff2499951ada0e44df87bc153430412
“me <sy@solitary.com>
2020-06-13 17:35:29 +0800

    sy

---
+++ src/main/java/com/dt/demospringboot_redis01/pojo/Liu.java
@@ -0,0 +1,13 @@
+package com.dt.demospringboot_redis01.pojo;
+
+/**
+ * @Title:
+ * @Package
+ * @Description:
+ * @author: 宋扬
+ * @date:
+ */
+public class Liu {
+    private int id;
+    private String name="宋扬";
+}
